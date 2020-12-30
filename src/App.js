fb84d8af0c1eb89868b521ceed73cac7c774e301
waranat suttikarn <waranat.cpe@gmail.com>
2020-12-30 10:33:49 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const liff = window.liff;
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      userLineID: '',
+      pictureUrl: ''
+    };
+  }
+
+  componentDidMount = async() => {
+    await liff.init({ liffId: `1655534752-05NG4BxE` }).catch(err=>{throw err});
+    if (liff.isLoggedIn()) {
+      let getProfile = await liff.getProfile();
+      this.setState({
+        name: getProfile.displayName,
+        userLineID: getProfile.userId,
+        pictureUrl: getProfile.pictureUrl,
+      });
+    }else{
+      liff.login();
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <form>
+          <img className="img img-circle" width="150" alt='pic' src={this.state.pictureUrl} />
+          <p>
+            <label>
+              ชื่อ-นามสกุล:
+              <input className="form-control" type="text" name="name" value={this.state.name}/>
+            </label>
+            </p>
+            <p>
+            <label>
+              เลขบัตรประชาชน:
+              <input className="form-control" type="text" name="citizen_id"/>
+            </label>
+            </p>
+            <p>
+            <label>
+              LineID:
+              <input className="form-control" type="text" name="line_id" value={this.state.userLineID}/>
+            </label>
+            </p>
+            <input type="submit" className="btn btn-success" value="ลงทะเบียน" />
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
